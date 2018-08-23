7c10d40485613b5e1f28b614d77d1e741ae9d9a2
Elvis Morales <elvismdev@gmail.com>
2018-08-22 23:52:31 -0700

    Using arrow functions all the way.

--- app_fm.js
+++ app_fm.js
@@ -5,17 +5,16 @@ const forever = require('forever-monitor');
 
 const wia = new (forever.Monitor)('app.js');
 
-// wia.on('restart', function () {
-// 	console.log('app.js has restarted');
-// 	wia.restart();
-// });
-
-wia.on('start', function () {
+wia.on('start', () => {
 	DEBUG.log(pkg.name + ' has started.');
 });
 
-// wia.on('exit', function () {
-// 	console.log('app.js has exited after 3 restarts');
-// });
+wia.on('restart', () => {
+	DEBUG.log(pkg.name + ' has restarted');
+});
+
+wia.on('stderr', () => {
+	console.log('app.js has exited after 3 restarts');
+});
 
 wia.start();
--- debug.js
+++ debug.js
@@ -1,7 +1,7 @@
 // Debug to console with date.
-module.exports = (function(){
-	var timestamp = function(){};
-	timestamp.toString = function(){
+module.exports = ( () => {
+	var timestamp = () => {};
+	timestamp.toString = () => {
 		let options = { weekday: "short", month: "short", day: "2-digit", hour12: false, year: "numeric" };
 		return "[" + (new Date).toLocaleTimeString('en-US', options).replace(/,/g, '') + "]";
 	};
